ad792c371a94c227463e47ce12dd1a621c36d8d7
elizi <mdzilkadri1@gmail.com>
2026-06-16 02:12:10 +0700

    fix mahasiswa
    .

--- src/app/mahasiswa/mahasiswa.ts
+++ src/app/mahasiswa/mahasiswa.ts
@@ -17,8 +17,8 @@ declare const $: any;
 export class Mahasiswa implements AfterViewInit {
 
   table1: any;
-mahasiswa: any;
- private resizeTimeout: any;
+  mahasiswa: any;
+  private resizeTimeout: any;
 
   constructor(private http: HttpClient, private renderer: Renderer2) {}
 
@@ -31,6 +31,7 @@ mahasiswa: any;
       }, 300); // debounce 300ms agar tidak terlalu sering
     });
   }
+
   destroyAndRebind(): void {
     if (this.table1) {
       this.table1.destroy();
@@ -41,96 +42,96 @@ mahasiswa: any;
   }
 
   bindMahasiswa(): void {
-  this.http.get("https://stmikpontianak.cloud/011100862/tampilMahasiswa.php")
-    .subscribe((data: any) => {
+    this.http.get("https://stmikpontianak.cloud/011100862/tampilMahasiswa.php")
+      .subscribe((data: any) => {
 
-      const isMobile = window.innerWidth < 768;
+        const isMobile = window.innerWidth < 768;
 
-      if (!this.table1) {
-        this.table1 = $('#datatable-mahasiswa').DataTable({
-          responsive: false,
-          lengthChange: true,
-          autoWidth: false,
-          scrollX: true,
-          columnDefs: isMobile
-            ? [{ targets: [2, 3, 4, 5, 6, 7, 8], visible: false }]
-             : [{ targets: [0], visible: false },{ targets: [5], width: '80px' }] 
+        if (!this.table1) {
+          this.table1 = $('#datatable-mahasiswa').DataTable({
+            responsive: false,
+            lengthChange: true,
+            autoWidth: false,
+            scrollX: true,
+            columnDefs: isMobile
+              ? [{ targets: [2, 3, 4, 5, 6, 7, 8], visible: false }]
+              : [{ targets: [0], visible: false },{ targets: [5], width: '80px' }] 
+          });
+
+          if (isMobile) {
+            $('#datatable-mahasiswa tbody').on('click', '.expand-btn', (event: any) => {
+              const tr = $(event.currentTarget).closest('tr');
+              const row = this.table1.row(tr);
+
+              if (row.child.isShown()) {
+                row.child.hide();
+                tr.find('.expand-btn').html('&#9654;');
+                tr.removeClass('shown');
+              } else {
+                const d = row.data();
+                row.child(this.formatDetail(d)).show();
+                tr.find('.expand-btn').html('&#9660;');
+                tr.addClass('shown');
+              }
+            });
+          }
+
+        } else {
+          this.table1.clear();
+        }
+
+        data.forEach((mhs: any) => {
+          const tempatTanggalLahir = `${mhs.TempatLahir}, ${mhs.TanggalLahir}`;
+          const jenisKelaminFormatted = mhs.JenisKelamin.toLowerCase() === 'perempuan'
+            ? `Perempuan <i class='fas fa-venus text-danger'></i>`
+            : `Laki-laki <i class='fas fa-mars text-primary'></i>`;
+
+          if (isMobile) {
+            this.table1.row.add([
+              '<button class="expand-btn btn btn-sm btn-outline-secondary py-0 px-1">&#9654;</button>',
+              mhs.NIM,
+              mhs.Nama,
+              jenisKelaminFormatted,
+              tempatTanggalLahir,
+              mhs.JP,
+              mhs.Alamat,
+              mhs.StatusNikah,
+              mhs.TahunMasuk
+            ]);
+          } else {
+            // Desktop: row normal tanpa kolom tombol expand
+            this.table1.row.add([
+              '', 
+              mhs.NIM,
+              mhs.Nama,
+              jenisKelaminFormatted,
+              tempatTanggalLahir,
+              mhs.JP,
+              mhs.Alamat,
+              mhs.StatusNikah,
+              mhs.TahunMasuk
+            ]);
+          }
         });
 
-        if (isMobile) {
-          $('#datatable-mahasiswa tbody').on('click', '.expand-btn', (event: any) => {
-            const tr = $(event.currentTarget).closest('tr');
-            const row = this.table1.row(tr);
-
-            if (row.child.isShown()) {
-              row.child.hide();
-              tr.find('.expand-btn').html('&#9654;');
-              tr.removeClass('shown');
-            } else {
-              const d = row.data();
-              row.child(this.formatDetail(d)).show();
-              tr.find('.expand-btn').html('&#9660;');
-              tr.addClass('shown');
-            }
-          });
-        }
-
-      } else {
-        this.table1.clear();
-      }
-
-      data.forEach((mhs: any) => {
-        const tempatTanggalLahir = `${mhs.TempatLahir}, ${mhs.TanggalLahir}`;
-        const jenisKelaminFormatted = mhs.JenisKelamin.toLowerCase() === 'perempuan'
-          ? `Perempuan <i class='fas fa-venus text-danger'></i>`
-          : `Laki-laki <i class='fas fa-mars text-primary'></i>`;
-
-        if (isMobile) {
-          this.table1.row.add([
-            '<button class="expand-btn btn btn-sm btn-outline-secondary py-0 px-1">&#9654;</button>',
-            mhs.NIM,
-            mhs.Nama,
-            jenisKelaminFormatted,
-            tempatTanggalLahir,
-            mhs.JP,
-            mhs.Alamat,
-            mhs.StatusNikah,
-            mhs.TahunMasuk
-          ]);
-        } else {
-          // Desktop: row normal tanpa kolom tombol expand
-          this.table1.row.add([
-            '', 
-            mhs.NIM,
-            mhs.Nama,
-            jenisKelaminFormatted,
-            tempatTanggalLahir,
-            mhs.JP,
-            mhs.Alamat,
-            mhs.StatusNikah,
-            mhs.TahunMasuk
-          ]);
-        }
+        this.table1.draw();
       });
+  }
 
-      this.table1.draw();
-    });
-}
   formatDetail(d: any): string {
-  return `
-    <table class="table table-sm table-borderless mb-0 ml-3">
-      <tr><td><b>Nama</b></td><td>: ${d[2]}</td></tr>
-      <tr><td><b>Jenis Kelamin</b></td><td>: ${d[3]}</td></tr>
-      <tr><td><b>Tempat, Tgl Lahir</b></td><td>: ${d[4]}</td></tr>
-      <tr><td><b>JP</b></td><td>: ${d[5]}</td></tr>
-      <tr><td><b>Alamat</b></td><td>: ${d[6]}</td></tr>
-      <tr><td><b>Status</b></td><td>: ${d[7]}</td></tr>
-      <tr><td><b>Tahun Masuk</b></td><td>: ${d[8]}</td></tr>
-    </table>`;
-}
+    return `
+      <table class="table table-sm table-borderless mb-0 ml-3">
+        <tr><td><b>Nama</b></td><td>: ${d[2]}</td></tr>
+        <tr><td><b>Jenis Kelamin</b></td><td>: ${d[3]}</td></tr>
+        <tr><td><b>Tempat, Tgl Lahir</b></td><td>: ${d[4]}</td></tr>
+        <tr><td><b>JP</b></td><td>: ${d[5]}</td></tr>
+        <tr><td><b>Alamat</b></td><td>: ${d[6]}</td></tr>
+        <tr><td><b>Status</b></td><td>: ${d[7]}</td></tr>
+        <tr><td><b>Tahun Masuk</b></td><td>: ${d[8]}</td></tr>
+      </table>`;
+  }
 
   postRecord(): void {
-
     var alamat = $("#alamatText").val();
     var jenisKelamin = $("#jenisKelaminSelect").val()?.toString().trim();
     var jp = $("#jpSelect").val();
@@ -160,15 +161,13 @@ mahasiswa: any;
       "&tanggalLahir=" + encodeURIComponent(tanggalLahir!) +
       "&tempatLahir=" + encodeURIComponent(tempatLahir!);
 
-    console.log("URL DIKIRIM:", url); // Debug
+    console.log("URL DIKIRIM:", url);
 
     this.http.get(url).subscribe((data: any) => {
-
       alert(data.status + " --> " + data.message);
 
-      this.bindMahasiswa();
+      this.destroyAndRebind();
 
-      // FIX ID MODAL
       $("#tambahModal").modal("hide");
     });
   }
